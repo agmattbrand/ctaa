5b855c7052f002af80cc8738733d5663d708fe0a
Matt Brand <agmattbrand@yahoo.com>
2011-08-31 14:46:28 -0400

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ctaa::Application.config.secret_token = '91789f8afac23e892d608dea28ef0b0dbee4827d721fed2aae7a75270762bc7fec7958173689fe39e57ff0f62097437ebcab762d4abacac29ee6258fe88a1344'
